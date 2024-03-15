ff661b9102ed6d8f7047e711e3cd8f772863987f
tanaka <152840606+kraw-t@users.noreply.github.com>
2024-03-15 13:55:04 +0900

    report: add time calculation logic 03/03_challenge

--- 03_containers_and_templates/03_challenge/README.md
+++ 03_containers_and_templates/03_challenge/README.md
@@ -4,43 +4,12 @@
 
 ## 結果
 
-### vectorの場合
-`Stack<uint32_t, std::vector<uint32_t>> stack;`
-
 ```
-ttanaka@MS1-ENV:~/ms1/05_cpp-02-ja/03_containers_and_templates/03_challenge$ g++ -std=c++17 -O0 main.cpp
-ttanaka@MS1-ENV:~/ms1/05_cpp-02-ja/03_containers_and_templates/03_challenge$ time ./a.out 
-
-real    0m4.851s
-user    0m4.741s
-sys     0m0.110s
-```
-
-### listの場合
-`Stack<uint32_t, std::list<uint32_t>> stack;`
-
-```
-ttanaka@MS1-ENV:~/ms1/05_cpp-02-ja/03_containers_and_templates/03_challenge$ g++ -std=c++17 -O0 main.cpp
-ttanaka@MS1-ENV:~/ms1/05_cpp-02-ja/03_containers_and_templates/03_challenge$ time ./a.out 
-
-real    0m22.798s
-user    0m20.387s
-sys     0m2.411s
-```
-
-### dequeの場合
-`Stack<uint32_t, std::deque<uint32_t>> stack;`
-
-```
-ttanaka@MS1-ENV:~/ms1/05_cpp-02-ja/03_containers_and_templates/03_challenge$ g++ -std=c++17 -O0 main.cpp
-ttanaka@MS1-ENV:~/ms1/05_cpp-02-ja/03_containers_and_templates/03_challenge$ time ./a.out 
-
-real    0m8.617s
-user    0m8.412s
-sys     0m0.200s
+elapsed time(vector): 964[ms]
+elapsed time(list): 1863[ms]
+elapsed time(deque): 646[ms]
 ```
 
 ## 最適なコンテナ
 
-先頭／末尾への追加・削除が早い`std::deque`が最も速いと予想したが、`std::vector`が最も速い結果となった。
-頻繁な要素の追加・削除ではなく、追加→削除の一方向で行ったため、`std::vector`の連続的なメモリ配置が高速化により寄与したのかもしれない。
+適していると予想した通り、先頭／末尾への追加・削除が早い`std::deque`が最も速い結果となった。
--- 03_containers_and_templates/03_challenge/main.cpp
+++ 03_containers_and_templates/03_challenge/main.cpp
@@ -1,13 +1,19 @@
 #include <vector>
 #include <list>
 #include <deque>
+#include <chrono>
+#include <string>
 #include "../02_challenge/stack.hpp"
 
-int main() {
-  Stack<uint32_t, std::deque<uint32_t>> stack;
+template<typename U>
+void push_pop_and_disp_calc_time(U& stack, std::string name) {
+  std::chrono::high_resolution_clock::time_point start, end;
+
+  // 時間計測開始
+  start = std::chrono::high_resolution_clock::now(); // 計測開始時間
 
   // add data to stack
-  for (uint32_t i = 0; i < 100000000; i++) {
+  for (uint32_t i = 0; i < 10000000; i++) {
     stack.push(i);
   }
 
@@ -18,5 +24,22 @@ int main() {
     stack.pop();
   }
 
+  // 計測終了と経過時間の表示
+  end = std::chrono::high_resolution_clock::now();  // 計測終了時間
+  double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count(); //処理に要した時間をミリ秒に変換
+  std::cout << "elapsed time(" << name << "): " << elapsed << "[ms]" << std::endl;
+  
+  return;
+}
+
+int main() {
+  Stack<uint32_t, std::vector<uint32_t>> stack_v;
+  push_pop_and_disp_calc_time<Stack<uint32_t, std::vector<uint32_t>>>(stack_v, "vector");
+
+  Stack<uint32_t, std::list<uint32_t>> stack_l;
+  push_pop_and_disp_calc_time<Stack<uint32_t, std::list<uint32_t>>>(stack_l, "list");
+
+  Stack<uint32_t, std::deque<uint32_t>> stack_d;
+  push_pop_and_disp_calc_time<Stack<uint32_t, std::deque<uint32_t>>>(stack_d, "deque");
   return 0;
 }
